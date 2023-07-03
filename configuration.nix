690a716244a74309cf05d6ff06c8b4a4eec87256
Yanda Huang <realyanda@hey.com>
2023-07-03 14:44:26 -0700

    Clean up dup user settings. Added cachix

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,19 @@
   # nix.package = pkgs.nix;
 
   # Necessary for using flakes on this system.
-  nix.settings.experimental-features = "nix-command flakes";
+  nix = {
+    settings = {
+      experimental-features = "nix-command flakes";
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+  };
 
   # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh.enable = true; # default shell on catalina
--- home.nix
+++ home.nix
@@ -5,11 +5,6 @@
     flake-inputs.nix-doom-emacs.hmModule
   ];
 
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = "yanda";
-  home.homeDirectory = "/Users/yanda";
-
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
