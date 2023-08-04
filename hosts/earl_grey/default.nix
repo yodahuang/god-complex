0a0c471fe7b7bc436508b2801204c3ed99c5fcd8
Yanda Huang <realyanda@hey.com>
2023-08-04 05:37:07 +0000

    AdGuard is working

--- hosts/earl_grey/default.nix
+++ hosts/earl_grey/default.nix
@@ -2,7 +2,6 @@
 {
   imports= [
     ./hardware-configuration.nix
-    ./adguard_home.nix
   ];
 
   # Use uboot.
@@ -32,5 +31,36 @@
   };
 
   system.stateVersion = "23.05";
+  
+  # Services.
+  services.adguardhome = {
+    enable = true;
+    mutableSettings = false;
+    settings = {
+      bind_host = "0.0.0.0";
+      bind_port = 1080;
+      dns = {
+        bind_hosts = [ "0.0.0.0" ];
+        port = 53;
+        upstream_dns = [
+          "https://dns.cloudflare.com/dns-query"
+        ];
+        filters = [
+          {
+            enabled = true;
+            url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_1.txt";
+            name = "AdGuard DNS filter";
+            id = 1;
+          }
+          {
+            enabled = true;
+            url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_29.txt";
+            name = "CHN: AdRules DNS List";
+            id = 1690779191;
+          }
+        ];
+      };
+    };
+  };
   
 }
